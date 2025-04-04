af88947029b51912dd759a7e71f7225478dc7b5f
Youregonex <Anton.shugaev.a@gmail.com>
2025-04-04 19:56:39 +0400

    Android fix

--- Assets/Scripts/Misc/WaterScreenSizeFitter.cs
+++ Assets/Scripts/Misc/WaterScreenSizeFitter.cs
@@ -6,12 +6,37 @@ namespace Youregone.Utils
     {
         [CustomHeader("Settings")]
         [SerializeField] private Camera _waterReflctionCamera;
+        [SerializeField] private Transform _waterVisualTransform;
 
-        private void Awake()
+        private void Start()
         {
-            float worldScreenWidth = Camera.main.orthographicSize * 2 * Screen.width / Screen.height;
-            float parameter = 100f;
-            transform.localScale = new Vector3(worldScreenWidth * parameter, transform.localScale.y, transform.localScale.z);
+            float waterVisualDefaultAspectRatio = _waterVisualTransform.localScale.x / _waterVisualTransform.localScale.y;
+            float cameraHeight = Camera.main.orthographicSize * 2;
+            float worldScreenWidth = cameraHeight * Screen.width / Screen.height;
+            float newHeight = worldScreenWidth / waterVisualDefaultAspectRatio;
+            float scaleCoef = 100f;
+
+            StretchSpriteDownOnly(_waterVisualTransform, newHeight * scaleCoef);
+
+            _waterVisualTransform.localScale = new Vector2(
+                worldScreenWidth * scaleCoef,
+                newHeight * scaleCoef);
+
+
+            float waterReflectionCameraBottomEdge = _waterReflctionCamera.transform.position.y - _waterReflctionCamera.orthographicSize;
+            transform.position = new Vector2(_waterReflctionCamera.transform.position.x, waterReflectionCameraBottomEdge);
+        }
+
+        void StretchSpriteDownOnly(Transform stretchObj, float newScaleY)
+        {
+            float originalScaleY = stretchObj.localScale.y;
+            float deltaScaleY = newScaleY - originalScaleY;
+
+            SpriteRenderer sr = stretchObj.GetComponent<SpriteRenderer>();
+            float spriteHeight = sr.bounds.size.y / originalScaleY;
+
+            float spriteHeightCoef = -.5f;
+            stretchObj.position += new Vector3(0f, spriteHeightCoef * spriteHeight * deltaScaleY, 0f);
         }
     }
 }
