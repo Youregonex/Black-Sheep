b7932272be2194e9dae89ef54e99e54114acda80
Subculturist <su6culturist@gmail.com>
2025-05-17 00:43:17 +0300

    Changed Tileset / Chunks

---
+++ Assets/Tiled/Tileset/NewTileset.tsx
@@ -0,0 +1,654 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="NewTileset" tilewidth="24" tileheight="24" tilecount="418" columns="38">
+ <image source="NewTileset.png" width="912" height="264"/>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="88" duration="100"/>
+   <frame tileid="92" duration="100"/>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="100" duration="100"/>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="108" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="93" duration="100"/>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="109" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="82">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="94" duration="100"/>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="110" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="83">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="87" duration="100"/>
+   <frame tileid="91" duration="100"/>
+   <frame tileid="95" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="103" duration="100"/>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="111" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="87">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="92">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="95">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="107">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="109">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 24,5 24,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="160" duration="100"/>
+   <frame tileid="164" duration="100"/>
+   <frame tileid="168" duration="100"/>
+   <frame tileid="172" duration="100"/>
+   <frame tileid="176" duration="100"/>
+   <frame tileid="180" duration="100"/>
+   <frame tileid="184" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="156">
+  <animation>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="160" duration="100"/>
+   <frame tileid="164" duration="100"/>
+   <frame tileid="168" duration="100"/>
+   <frame tileid="172" duration="100"/>
+   <frame tileid="176" duration="100"/>
+   <frame tileid="180" duration="100"/>
+   <frame tileid="184" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="157">
+  <animation>
+   <frame tileid="157" duration="100"/>
+   <frame tileid="161" duration="100"/>
+   <frame tileid="165" duration="100"/>
+   <frame tileid="169" duration="100"/>
+   <frame tileid="173" duration="100"/>
+   <frame tileid="177" duration="100"/>
+   <frame tileid="181" duration="100"/>
+   <frame tileid="185" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="158">
+  <animation>
+   <frame tileid="158" duration="100"/>
+   <frame tileid="162" duration="100"/>
+   <frame tileid="166" duration="100"/>
+   <frame tileid="170" duration="100"/>
+   <frame tileid="174" duration="100"/>
+   <frame tileid="178" duration="100"/>
+   <frame tileid="182" duration="100"/>
+   <frame tileid="186" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="159">
+  <animation>
+   <frame tileid="159" duration="100"/>
+   <frame tileid="163" duration="100"/>
+   <frame tileid="167" duration="100"/>
+   <frame tileid="171" duration="100"/>
+   <frame tileid="175" duration="100"/>
+   <frame tileid="179" duration="100"/>
+   <frame tileid="183" duration="100"/>
+   <frame tileid="187" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="190">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 24,0 24,5 0,5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -24,5 -24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="193">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="194">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="194" duration="100"/>
+   <frame tileid="198" duration="100"/>
+   <frame tileid="202" duration="100"/>
+   <frame tileid="206" duration="100"/>
+   <frame tileid="210" duration="100"/>
+   <frame tileid="214" duration="100"/>
+   <frame tileid="218" duration="100"/>
+   <frame tileid="222" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="195">
+  <animation>
+   <frame tileid="195" duration="100"/>
+   <frame tileid="199" duration="100"/>
+   <frame tileid="203" duration="100"/>
+   <frame tileid="207" duration="100"/>
+   <frame tileid="211" duration="100"/>
+   <frame tileid="215" duration="100"/>
+   <frame tileid="219" duration="100"/>
+   <frame tileid="223" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="196">
+  <animation>
+   <frame tileid="196" duration="100"/>
+   <frame tileid="200" duration="100"/>
+   <frame tileid="204" duration="100"/>
+   <frame tileid="208" duration="100"/>
+   <frame tileid="212" duration="100"/>
+   <frame tileid="216" duration="100"/>
+   <frame tileid="220" duration="100"/>
+   <frame tileid="224" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="197">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="5">
+    <polygon points="0,0 -10,0 -10,-5 0,-5"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="197" duration="100"/>
+   <frame tileid="201" duration="100"/>
+   <frame tileid="205" duration="100"/>
+   <frame tileid="209" duration="100"/>
+   <frame tileid="213" duration="100"/>
+   <frame tileid="217" duration="100"/>
+   <frame tileid="221" duration="100"/>
+   <frame tileid="225" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="198">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="201">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="202">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="205">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="206">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="209">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="210">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="213">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="214">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="217">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="218">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="221">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="222">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,5 10,5 10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="225">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0">
+    <polygon points="0,0 0,5 -10,5 -10,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="232">
+  <animation>
+   <frame tileid="232" duration="100"/>
+   <frame tileid="236" duration="100"/>
+   <frame tileid="240" duration="100"/>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="248" duration="100"/>
+   <frame tileid="252" duration="100"/>
+   <frame tileid="256" duration="100"/>
+   <frame tileid="260" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="233">
+  <animation>
+   <frame tileid="233" duration="100"/>
+   <frame tileid="237" duration="100"/>
+   <frame tileid="241" duration="100"/>
+   <frame tileid="245" duration="100"/>
+   <frame tileid="249" duration="100"/>
+   <frame tileid="253" duration="100"/>
+   <frame tileid="257" duration="100"/>
+   <frame tileid="261" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="234">
+  <animation>
+   <frame tileid="234" duration="100"/>
+   <frame tileid="238" duration="100"/>
+   <frame tileid="242" duration="100"/>
+   <frame tileid="246" duration="100"/>
+   <frame tileid="250" duration="100"/>
+   <frame tileid="254" duration="100"/>
+   <frame tileid="258" duration="100"/>
+   <frame tileid="262" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="235">
+  <animation>
+   <frame tileid="235" duration="100"/>
+   <frame tileid="239" duration="100"/>
+   <frame tileid="243" duration="100"/>
+   <frame tileid="247" duration="100"/>
+   <frame tileid="251" duration="100"/>
+   <frame tileid="255" duration="100"/>
+   <frame tileid="259" duration="100"/>
+   <frame tileid="263" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="270">
+  <animation>
+   <frame tileid="270" duration="100"/>
+   <frame tileid="274" duration="100"/>
+   <frame tileid="278" duration="100"/>
+   <frame tileid="282" duration="100"/>
+   <frame tileid="286" duration="100"/>
+   <frame tileid="290" duration="100"/>
+   <frame tileid="294" duration="100"/>
+   <frame tileid="298" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="271">
+  <animation>
+   <frame tileid="271" duration="100"/>
+   <frame tileid="275" duration="100"/>
+   <frame tileid="279" duration="100"/>
+   <frame tileid="283" duration="100"/>
+   <frame tileid="287" duration="100"/>
+   <frame tileid="291" duration="100"/>
+   <frame tileid="295" duration="100"/>
+   <frame tileid="299" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="272">
+  <animation>
+   <frame tileid="272" duration="100"/>
+   <frame tileid="276" duration="100"/>
+   <frame tileid="280" duration="100"/>
+   <frame tileid="284" duration="100"/>
+   <frame tileid="288" duration="100"/>
+   <frame tileid="292" duration="100"/>
+   <frame tileid="296" duration="100"/>
+   <frame tileid="300" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="273">
+  <animation>
+   <frame tileid="273" duration="100"/>
+   <frame tileid="277" duration="100"/>
+   <frame tileid="281" duration="100"/>
+   <frame tileid="285" duration="100"/>
+   <frame tileid="289" duration="100"/>
+   <frame tileid="293" duration="100"/>
+   <frame tileid="297" duration="100"/>
+   <frame tileid="301" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="308">
+  <animation>
+   <frame tileid="308" duration="100"/>
+   <frame tileid="312" duration="100"/>
+   <frame tileid="316" duration="100"/>
+   <frame tileid="320" duration="100"/>
+   <frame tileid="324" duration="100"/>
+   <frame tileid="328" duration="100"/>
+   <frame tileid="332" duration="100"/>
+   <frame tileid="336" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="309">
+  <animation>
+   <frame tileid="309" duration="100"/>
+   <frame tileid="317" duration="100"/>
+   <frame tileid="313" duration="100"/>
+   <frame tileid="321" duration="100"/>
+   <frame tileid="325" duration="100"/>
+   <frame tileid="329" duration="100"/>
+   <frame tileid="333" duration="100"/>
+   <frame tileid="337" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="310">
+  <animation>
+   <frame tileid="310" duration="100"/>
+   <frame tileid="314" duration="100"/>
+   <frame tileid="318" duration="100"/>
+   <frame tileid="322" duration="100"/>
+   <frame tileid="326" duration="100"/>
+   <frame tileid="330" duration="100"/>
+   <frame tileid="334" duration="100"/>
+   <frame tileid="338" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="311">
+  <animation>
+   <frame tileid="311" duration="100"/>
+   <frame tileid="315" duration="100"/>
+   <frame tileid="319" duration="100"/>
+   <frame tileid="323" duration="100"/>
+   <frame tileid="327" duration="100"/>
+   <frame tileid="331" duration="100"/>
+   <frame tileid="335" duration="100"/>
+   <frame tileid="339" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
